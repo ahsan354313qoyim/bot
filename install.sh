c4bc8936ecb6c6985272da8952c221955b6222a0
ahsan354 <75293224+ahsan354@users.noreply.github.com>
2020-12-14 12:02:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
-apt update && apt upgrade
+apt update
+apt upgrade
+apt install
+pkg update && pkg upgrade
 pkg install wget
 pkg install ffmpeg
 pkg install nodejs
+pkg install tesseract
 npm i -g cwebp
 npm i -g ytdl 
 npm i
 npm i got
-pkg install tesseract
